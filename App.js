69cb178a0c55a1c72e43aa621fa1bda5876da8bb
Жигжидсүрэн Амарсайхан <HP233023@M301-CC0140.local>
2023-07-17 15:39:46 +0800

    asd

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { Card, TextInput, Button } from "react-native-paper";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.top}>
+        <Text style={{ fontSize: 30 }}>Chat</Text>
+        <Card style={styles.card}>
+          <Card.Content>
+            <Text>hello</Text>
+          </Card.Content>
+        </Card>
+      </View>
+      <View style={styles.bot}>
+        <TextInput style={styles.inp} mode="outlined"></TextInput>
+        <Button
+          mode="contained"
+          style={styles.button}
+          icon="send"
+          onPress={() => alert("clicked")}
+        ></Button>
+      </View>
     </View>
   );
 }
@@ -13,8 +28,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+    paddingTop: 40,
+    paddingBottom: 40,
+    paddingLeft: 26,
+  },
+  top: {
+    gap: 10,
+  },
+  inp: {
+    width: 300,
+  },
+  button: {
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+    marginTop: 6,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 15,
+  },
+  bot: {
+    flexDirection: "row",
+    gap: 5,
+  },
+  card: {
+    width: 300,
   },
 });
